beecb3e3479751e11983bf9a8e00d845b7e851e6
Darcy Abell <Darcy@thethreethousands.com>
2024-11-07 15:25:41 +1100

    Trim quotes when pulling file name from git status - git will return filenames inside quotes if the file name contains spaces

--- Source/GitSourceControl/Private/GitSourceControlUtils.cpp
+++ Source/GitSourceControl/Private/GitSourceControlUtils.cpp
@@ -965,16 +965,18 @@ R  Content/Textures/T_Perlin_Noise_M.uasset -> Content/Textures/T_Perlin_Noise_M
 static FString FilenameFromGitStatus(const FString& InResult)
 {
 	int32 RenameIndex;
+	FString result;
 	if (InResult.FindLastChar('>', RenameIndex))
 	{
 		// Extract only the second part of a rename "from -> to"
-		return InResult.RightChop(RenameIndex + 2);
+		result = InResult.RightChop(RenameIndex + 2);
 	}
 	else
 	{
 		// Extract the relative filename from the Git status result (after the 2 letters status and 1 space)
-		return InResult.RightChop(3);
+		result = InResult.RightChop(3);
 	}
+	return result.TrimQuotes();
 }
 
 /** Match the relative filename of a Git status result with a provided absolute filename */
